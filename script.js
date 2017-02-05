7d47adbf066b7d43becf0f08cb041290cd704918
Ozgun Bal <ozgunbal@gmail.com>
2017-02-05 15:46:07 +0300

    add javascript and edit transform scale

---
+++ script.js
@@ -0,0 +1,17 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; //stops the function
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; //skip
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
