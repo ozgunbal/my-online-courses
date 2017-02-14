4f22daf333aba385feb92e07a0258d9073bebaaf
Ozgun Bal <ozgunbal@gmail.com>
2017-02-14 08:58:55 +0300

    final solution of the day 9 challenge

--- Day 9 - Dev Tools Domination/script.js
+++ Day 9 - Dev Tools Domination/script.js
@@ -7,25 +7,59 @@ function makeGreen() {
 }
 
 // Regular
+console.log('hello');
 
 // Interpolated
+console.log('Hello I am a %s string!', 'LeL');
 
 // Styled
+console.log('%c I am a some great text', 'font-size:50px')
 
 // warning!
+console.warn('Oh Nooo');
 
 // Error :|
+console.error('Shit!');
 
 // Info
+console.info('I am an info');
 
 // Testing
+const p = document.querySelector('p');
+console.assert(p.classList.contains('ouch'), 'That is wrong!');
 
 // clearing
+console.clear();
 
 // Viewing DOM Elements
+console.dir(p);
+
+console.clear();
 
 // Grouping together
+dogs.forEach(dog => {
+    console.groupCollapsed(`${dog.name}`)
+    console.log(`This is ${dog.name}`);
+    console.log(`${dog.name} is ${dog.age} years old`);
+    console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+    console.groupEnd();
+});
 
 // counting
+console.count('Wes');
+console.count('Wes');
+console.count('Steve');
+console.count('Wes');
 
 // timing
+console.time('fetching data');
+fetch('https://api.github.com/users/wesbos')
+  .then(data => data.json())
+  .then(data => {
+      console.timeEnd('fetching data');
+      console.log(data);
+  });
+
+// table
+console.table(dogs);
+
