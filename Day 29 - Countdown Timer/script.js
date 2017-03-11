ba2701994af0ed66b269e168bb9f3aa1077d291e
Ozgun Bal <ozgunbal@gmail.com>
2017-03-11 22:40:32 +0300

    final solution of the day 29 challenge

---
+++ Day 29 - Countdown Timer/script.js
@@ -0,0 +1,51 @@
+const timerDisplay = document.querySelector('.display__time-left');
+const endTimeDisplay = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+let countdown;
+
+function timer(seconds) {
+    // clear any existing timers
+    clearInterval(countdown);
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if(secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0':''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
+    document.title = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTimeDisplay.textContent = `Be Back at ${hour}:${minutes < 10 ? '0':''}${minutes}`;
+}
+
+function startTimer (){
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+})
